c685fce2ae43d8d764a22f337c2a0516fb5b5fa2
Eric Van <ggclarence@yahoo.com>
2022-03-01 07:56:57 -0600

    commit tree 2e8f83b721bd658944988cff496fb8b255a3d368

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
-
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const [btnStatus, setMode ] = useState(true)
+  
+  function handleClick(){
+    setMode((btnStatus)=> !btnStatus)
+    console.log(btnStatus)
+  }
+  
+  const appClass = btnStatus ? "App dark" : "App light"
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleClick}  >{btnStatus ? "Dark Mode": "Light Mode"}</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
@@ -20,3 +24,7 @@ function App() {
 }
 
 export default App;
+
+
+  // replace 'false' with a state variable that can be toggled between true and false
+  // this will be used for the Dark Mode Toggle feature
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [cartStatus, clickCart] = useState(false)
+  function clickHandler(){
+    clickCart((cartStatus)=> !cartStatus)
+    console.log(cartStatus)
+  }
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={cartStatus?"in-cart":"add"} onClick={clickHandler}>{cartStatus ? "Remove From Cart ":"Add to Cart"}</button>
     </li>
   );
 }
 
 export default Item;
+
+
+// In the Item component, when the user clicks the <button> element, the item should be added to their virtual cart. 
+// The way we'll show the user that the item is in the cart is by changing the className on the <li> element:
+// If the item is not in the cart, the <button> element's text should read "Add to Cart", and if the item is in the cart,
+// the <button> element's text should read "Remove From Cart". Naturally, you'll also need to add state to the Item component to solve this deliverable!
